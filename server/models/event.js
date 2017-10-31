45f24e2c44d7fedc8380a873bdf78d9655e38af5
KosherKoala <figueroahunter.08@gmail.com>
2017-10-31 09:20:45 -0400

    Calendar schema rework

--- server/models/event.js
+++ server/models/event.js
@@ -5,7 +5,6 @@ const Schema = mongoose.Schema;
 const Patient = require('./patient');
 
 const eventSchema = new Schema ({
-    eventId: Number,
     description: String,
     date: String,
     patient: { type: Schema.Types.ObjectId, ref: 'Patient' }, 
