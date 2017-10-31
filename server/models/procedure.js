6731d3acb8171e53c42f13d2df01a0455117273b
KosherKoala <figueroahunter.08@gmail.com>
2017-10-31 10:10:33 -0400

    Schema update

--- server/models/procedure.js
+++ server/models/procedure.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const procedureSchema = new Schema ({
+    doctor: { type: Schema.Types.ObjectId, ref: 'Doctor'},
     name: String,
     cost: Number,
     description: String,
