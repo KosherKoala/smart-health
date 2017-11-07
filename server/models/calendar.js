421396d90322fefcda0e13e8515aaad19e9a4031
Marcos Montes de Oca <Marcos@Marcoss-MacBook-Pro.local>
2017-11-07 12:39:33 -0500

    Fixed calendar routes.

--- server/models/calendar.js
+++ server/models/calendar.js
@@ -4,11 +4,18 @@ const Schema = mongoose.Schema;
 const autopopulate = require('mongoose-autopopulate');
 const Doctor = require('./doctor');
 const Appointment = require('./appointment');
+const Procedure = require('./procedure');
 
 const calendarSchema = new Schema ({
     
     doctor: { type: Schema.Types.ObjectId, ref: 'Doctor' },
     appointments: [{type: Schema.Types.ObjectId, ref: 'Appointment' }],
+    slots: {
+        startDate: Date,
+        endDate: Date,
+        procedure: { type: Schema.Types.ObjectId, ref: 'Procedure'},
+        reoccurrence: [String]
+    }
 });
 calendarSchema.plugin(autopopulate);
 
